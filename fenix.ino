125c65f02733938031b486198d4c7b21d87fabc1
Elitesniper06 <robert19092006@gmail.com>
2024-03-10 20:06:23 +0100

    Fenix-stick

---
+++ fenix.ino
@@ -0,0 +1,79 @@
+// #define STICK_C_PLUS
+#define STICK_C_PLUS2
+
+#if defined(STICK_C_PLUS)
+#include <M5StickCPlus.h>
+// -=-=- Display -=-=-
+String platformName = "StickC+";
+#define BIG_TEXT 4
+#define MEDIUM_TEXT 3
+#define SMALL_TEXT 2
+#define TINY_TEXT 1
+// -=-=- FEATURES -=-=-
+#define M5LED 10
+#define RTC
+#define AXP
+#define ACTIVE_LOW_IR
+#define ROTATION
+#define USE_EEPROM
+// -=-=- ALIASES -=-=-
+#define DISP M5.Lcd
+#define IRLED 9
+#define SPEAKER M5.Beep
+//  #define BITMAP M5.Lcd.drawBitmap(0, 0, 320, 240, NEMOMatrix) // This doesn't work, generates static.
+#define M5LED_ON LOW
+#define M5LED_OFF HIGH
+#endif
+
+#if defined(STICK_C_PLUS2)
+#include <M5StickCPlus2.h>
+// -=-=- Display -=-=-
+String platformName = "StickC+2";
+#define BIG_TEXT 4
+#define MEDIUM_TEXT 3
+#define SMALL_TEXT 2
+#define TINY_TEXT 1
+// -=-=- FEATURES -=-=-
+#define ACTIVE_LOW_IR
+#define M5LED 19
+#define ROTATION
+#define USE_EEPROM
+#define RTC
+#define PWRMGMT
+#define SPEAKER M5.Speaker
+#define SONG
+// -=-=- ALIASES -=-=-
+#define DISP M5.Lcd
+#define IRLED 19
+#define M5_BUTTON_MENU 35
+#define M5_BUTTON_HOME 37
+#define M5_BUTTON_RST 39
+#define BACKLIGHT 27
+#define MINBRIGHT 190
+#define M5LED_ON HIGH
+#define M5LED_OFF LOW
+#endif
+
+#if defined(RTC)
+    void clock_loop() {
+        DISP.setCursor(10, 10, 1);
+    #if defined(STICK_C_PLUS2)
+        auto dt = StickCP2.Rtc.getDateTime();
+        DISP.printf("%02d:%02d:%02d\n", dt.time.hours, dt.time.minutes, dt.time.seconds);
+    #else
+        M5.Rtc.GetBm8563Time();
+        DISP.printf("%02d:%02d:%02d\n", M5.Rtc.Hour, M5.Rtc.Minute, M5.Rtc.Second);
+    #endif
+        delay(250);
+    }
+#endif
+
+
+void setup() {
+    M5.begin();
+    M5.Lcd.setRotation(1);
+}
+
+void loop() {
+    clock_loop();
+}
